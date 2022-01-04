11f1502b11a9ccdff991028f19f73ca6317d0a7f
HaYeon <ha92062751@gmail.com>
2022-01-04 20:11:08 +0900

    init

---
+++ java/test.java
@@ -0,0 +1,21 @@
+package java;
+class test {
+  public static void main(String[] args) {
+    ThreadTest t = new ThreadTest();
+    t.start();
+  }
+}
+
+class ThreadTest extends Thread {
+  public void run() {
+    try {
+      for (int i = 0; i < 10; i++) {
+        Thread.sleep(200);
+        System.out.println("정용훈");
+      }
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.out.println(e);
+    }
+  }
+}
